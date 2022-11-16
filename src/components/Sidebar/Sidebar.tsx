76c3de0431e53c696c6862fc9190df9e51379a29
unknown <wnichols1865@gmail.com>
2022-11-15 20:10:29 -0500

    styling slider

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Button, Offcanvas } from "react-bootstrap";
 import './Sidebar.css';
 
-export function Sidebar() {
+export function Sidebar({ jokeCount, setJokeCount }: { jokeCount: number, setJokeCount: any }) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -14,12 +14,13 @@ export function Sidebar() {
                 Launch
             </Button>
 
-            <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas show={show} onHide={handleClose} placement="top" className="">
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>How many Jokes?</Offcanvas.Title>
                 </Offcanvas.Header>
-                <Offcanvas.Body className="debug">
-                    <input type="range" min="1" max="10" className="slider" />
+                <Offcanvas.Body className="d-flex flex-column align-items-center justify-content-center gap-4">
+                    <label className="slider-label">{jokeCount}</label>
+                    <input type="range" min="1" max="10" className="slider" onChange={(e: ChangeEvent<HTMLInputElement>) => setJokeCount(parseInt(e.target.value))} value={jokeCount} />
                 </Offcanvas.Body>
             </Offcanvas>
         </>
--- src/pages/JokePage/JokePage.tsx
+++ src/pages/JokePage/JokePage.tsx
@@ -1,29 +1,28 @@
+import { useState } from "react";
 import { Col, Container, Navbar, Row } from "react-bootstrap";
 import { Joke } from "../../components/Joke/Joke"
 import { Sidebar as Menu } from '../../components/Sidebar/Sidebar';
 
 export function JokePage() {
+    const [jokeCount, setJokeCount] = useState<number>(5);
+
     return (
         <>
 
             <Navbar className="py-2 bg-warning justify-content-between mb-2">
-                <Menu />
+                <Menu setJokeCount={setJokeCount} jokeCount={jokeCount} />
                 <h1>Jokes</h1>
             </Navbar>
             <Container>
                 <Row xs={1} md={2} lg={3}>
-                    <Col>
-                        <Joke />
-                    </Col>
-                    <Col>
-                        <Joke />
-                    </Col>
-                    <Col>
-                        <Joke />
-                    </Col>
-                    <Col>
-                        <Joke />
-                    </Col>
+                    {
+                        [...Array(jokeCount).keys()].map((el, idx) =>
+                        (
+                            <Col key={idx}>
+                                <Joke />
+                            </Col>
+                        )
+                        )}
                 </Row>
             </Container>
         </>
